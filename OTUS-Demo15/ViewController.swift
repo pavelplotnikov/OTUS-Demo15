cde280de803ba1d6c61abc5ec9a2b63e33e1aac5
Pavel Plotnikov <pplotnikov@gmail.com>
2021-03-01 23:33:02 +0300

    Initial commit

---
+++ OTUS-Demo15/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  OTUS-Demo1
+//
+//  Created by Pavel Plotnikov on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // view1.backgroundColor = UIColor(named: "gold")
+        // view2.backgroundColor = color
+    }
+
+
+}
+
